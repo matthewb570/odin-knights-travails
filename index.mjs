7f62f23c2c43ba180b8bfb13791334f25a1130d1
Matthew Burgess <boog397@gmail.com>
2024-11-17 22:02:29 -0600

    Add sudo code for knightMoves function

---
+++ index.mjs
@@ -0,0 +1,10 @@
+function knightMoves(start, end) {
+    /**
+     * Initialize queue with "start"
+     * While queue is not empty
+     *   Dequeue next coordinate array
+     *   If last item in coordinate array is "end", return coordinate array
+     *   Calculate all valid moves from last item in coordinate array
+     *   For each valid move, append to coordinate array and enqueue
+     */
+}
