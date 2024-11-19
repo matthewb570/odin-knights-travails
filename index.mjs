db9cce9af1ad9148968710f8b7fe6cc64161aabc
Matthew Burgess <boog397@gmail.com>
2024-11-18 20:50:58 -0600

    Add function to get valid knight moves for a particular coordinate

--- index.mjs
+++ index.mjs
@@ -8,3 +8,17 @@ function knightMoves(start, end) {
      *   For each valid move, append to coordinate array and enqueue
      */
 }
+
+function getValidKnightMovesForCoordinate(x, y) {
+    let validKnightMoves = new Array();
+    validKnightMoves.push([x + 2, y - 1], [x + 1, y - 2], [x - 1, y - 2], [x - 2, y - 1], [x - 2, y + 1], [x - 1, y + 2], [x + 1, y + 2], [x + 2, y + 1]);
+    validKnightMoves = validKnightMoves.filter((coordinate) => coordinate[0] >= 0 && coordinate[0] <= 7 && coordinate[1] >= 0 && coordinate[1] <= 7);
+
+    return validKnightMoves;
+}
+
+console.log(getValidKnightMovesForCoordinate(0, 7));
+console.log(getValidKnightMovesForCoordinate(3, 3));
+console.log(getValidKnightMovesForCoordinate(0, 0));
+console.log(getValidKnightMovesForCoordinate(7, 0));
+console.log(getValidKnightMovesForCoordinate(7, 7));
